51cb6cdd89937655ab828c7dc7a15fade6d50b91
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-01 18:57:00 +0500

    Final exp.c

--- src/s21_exp.c
+++ src/s21_exp.c
@@ -1,18 +1,37 @@
+#include <ieee754.h>
+
 #include "s21_math.h"
 
 long double s21_exp(double x) {
-    long double result = 1, temp = 1, prev_result = 0, epsilon = 1e-20;
-    int n = 1;
-    if (x == S21_NAN) {
-        result = S21_NAN;
-    } else if (x == S21_INF) {
-        result = S21_INF;
-    } else {
-        while (s21_fabs(result - prev_result) > epsilon) {
-            prev_result = result;
-            temp = temp * x / n++;
-            result += temp;
-        }
+  long double result = 1, temp = 1, prev_result = 0, epsilon = 1e-20, n = 1;
+  union ieee754_double x754;
+  union ieee754_double x754_full;
+  x754.d = x;
+
+  // (1 << 11) - 1 = 2047
+  x754_full.ieee.exponent = (1 << 11) - 1;
+
+  if ((x754.ieee_nan.exponent == x754_full.ieee.exponent) &&
+      (x754.ieee_nan.quiet_nan)) {
+    result = x;
+  } else if ((x754.ieee_nan.exponent == x754_full.ieee.exponent) &&
+             (x754.ieee_nan.mantissa0 == 0)) {
+    if (x754.ieee_nan.negative)
+      result = 0;
+    else
+      result = x;
+  } else {
+    x754.d = (double)s21_fabs(x);
+
+    while (s21_fabs(result - prev_result) > epsilon) {
+      prev_result = result;
+      temp *= x754.d / n++;
+      result += temp;
     }
-    return (double)result;
+
+    x754.d = x;
+    if (x754.ieee_nan.negative) result = 1 / (double)result;
+  }
+
+  return (double)result;
 }
