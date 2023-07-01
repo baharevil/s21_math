52270ecf185b7096362d1370a265dbdd1e4b3323
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-01 18:58:33 +0500

    Final exp.c

--- src/s21_exp.c
+++ src/s21_exp.c
@@ -4,10 +4,8 @@
 
 long double s21_exp(double x) {
   long double result = 1, temp = 1, prev_result = 0, epsilon = 1e-20, n = 1;
-  union ieee754_double x754;
-  union ieee754_double x754_full;
+  union ieee754_double x754, x754_full = {0};
   x754.d = x;
-
   // (1 << 11) - 1 = 2047
   x754_full.ieee.exponent = (1 << 11) - 1;
 
