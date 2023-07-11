96bacf6c483b58287bd853995be2df981d56b951
Bloodislav <vladhamrik@gmail.com>
2023-07-11 15:04:41 +0500

    Add second realization.

--- src/s21_fmod.c
+++ src/s21_fmod.c
@@ -1,10 +1,23 @@
-#include <ieee754.h>
-
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
+  // ВАРИАНТ_1
   long long int mod = (long long int)(x / y);
   long double result = x - (double)mod * y;
+
+  // ВАРИАНТ_2
+  // long double mod = -1, temp = 0, result = 0;
+  // long double copy = s21_fabs(x);
+
+  // while (temp < copy) {
+  //   mod++;
+  //   temp += y;
+  // }
+
+  // result = copy - mod * y;
+
+  // if (x < 0)
+  //   result *= -1;
 
   return (double)result;
 }
