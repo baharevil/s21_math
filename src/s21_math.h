45014bfea7b5d2bfa1ca42ad8a311a134e9dc775
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-12 18:16:46 +0500

    Add function s21_ceil + blanks for future functions.

--- src/s21_math.h
+++ src/s21_math.h
@@ -24,18 +24,26 @@
 #include <ieee754.h>
 
 int s21_abs(int x);
-long double s21_fabs(double x);
+long double s21_acos(double x);
+long double s21_asin(double x);
+long double s21_atan(double x);
+long double s21_ceil(double x);
+long double s21_cos(double x);
 long double s21_exp(double x);
-long double s21_log(double x);
+long double s21_fabs(double x);
+long double s21_floor(double x);
 long double s21_fmod(double x, double y);
+long double s21_log(double x);
 long double s21_pow(double base, double exponent);
+long double s21_sin(double x);
 long double s21_sqrt(double x);
+long double s21_tan(double x);
 
 // 1	int abs(int x)	                            | +
 // 2	long double acos(double x)	                | 
 // 3	long double asin(double x)	                | 
 // 4	long double atan(double x)	                | 
-// 5	long double ceil(double x)	                | 
+// 5	long double ceil(double x)	                | +
 // 6	long double cos(double x)	                | 
 // 7	long double exp(double x)	                | +
 // 8	long double fabs(double x)	                | +
