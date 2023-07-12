6f4bf972538af2a5cd3adab90ac53af219b1e817
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-12 18:45:58 +0500

    s21_ceil.c s21_floor.c v2.

--- src/function/s21_ceil.c
+++ src/function/s21_ceil.c
@@ -1,10 +1,24 @@
 #include "../s21_math.h"
 
 long double s21_ceil(double x) {
-    double mod = (double)s21_fmod(x, 1.0), result = x - mod;
+  double mod = 0, result = 0;
+  union ieee754_double copy_x, x754_full = {0};
 
-    if ((s21_fabs((double)mod) > 0) && x > 0) 
-        result++;
+  // Создаем копию
+  copy_x.d = x;
 
-    return result;
+  // (1 << 11) - 1 = 2047
+  x754_full.ieee.exponent = (1 << 11) - 1;
+
+  // Проверка х = +-inf
+  if ((copy_x.ieee_nan.exponent == x754_full.ieee.exponent) &&
+      (copy_x.ieee_nan.mantissa0 == 0))
+    result = x;
+
+  else {
+    mod = (double)s21_fmod(x, 1.0);
+    result = x - mod;
+    if ((s21_fabs((double)mod) > 0) && x > 0) result++;
+  }
+  return result;
 }
--- src/function/s21_floor.c
+++ src/function/s21_floor.c
@@ -1,10 +1,26 @@
 #include "../s21_math.h"
 
 long double s21_floor(double x) {
-    double mod = (double)s21_fmod(x, 1.0), result = x - mod;
+  double mod = 0, result = 0;
+  union ieee754_double copy_x, x754_full = {0};
 
-    if ((x < 0) && (s21_fabs(mod) > 0))
-        result--;
+  // Создаем копию
+  copy_x.d = x;
 
-    return result;
+  // (1 << 11) - 1 = 2047
+  x754_full.ieee.exponent = (1 << 11) - 1;
+
+  // Проверка х = +-inf
+  if ((copy_x.ieee_nan.exponent == x754_full.ieee.exponent) &&
+      (copy_x.ieee_nan.mantissa0 == 0))
+    result = x;
+
+  else {
+    mod = (double)s21_fmod(x, 1.0);
+    result = x - mod;
+
+    if ((x < 0) && (s21_fabs(mod) > 0)) result--;
+  }
+
+  return result;
 }
