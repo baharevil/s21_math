326d4e0231cec1bbe6360ca61ec64ee4c2973f93
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-12 16:12:37 +0500

    Corrected checks for base and exp.

--- src/s21_pow.c
+++ src/s21_pow.c
@@ -1,24 +1,36 @@
 #include <ieee754.h>
+#include <stdio.h>
 
 #include "s21_math.h"
 
 long double s21_pow(double base, double exp) {
   double result = 1, epsilon = 1e-10;
-  union ieee754_double x754, x754_full = {0};
+  union ieee754_double x754_base, x754_exp, x754_full = {0};
+  // union ieee754_double x754_base, x754_full = {0};
 
-  x754.d = (double)s21_fabs(base);
+  x754_base.d = (double)s21_fabs(base);
+  x754_exp.d = exp;
 
   // (1 << 11) - 1 = 2047
   x754_full.ieee.exponent = (1 << 11) - 1;
 
   // Проверка на бесконечность в base
-  if ((x754.ieee.exponent == x754_full.ieee.exponent) &&
-      (x754.ieee.mantissa0 == 0))
+  if ((x754_base.ieee.exponent == x754_full.ieee.exponent) &&
+      (x754_base.ieee.mantissa0 == 0))
     result = S21_INF;
 
+  // Проверка на бесконечность в exp
+  else if ((x754_exp.ieee.exponent == x754_full.ieee.exponent) &&
+      (x754_exp.ieee.mantissa0 == 0) && x754_base.d > 1.0) {
+      if(x754_exp.ieee.negative) 
+        result = 0.0;
+      else 
+        result = S21_INF;
+      }
+
   // Проверка на 1 как в pow так и в base
-  else if (s21_fabs((double)(exp - 1)) < epsilon ||
-           s21_fabs((double)(base - 1)) < epsilon)
+  else if (s21_fabs((double)(exp - 1.0)) < epsilon ||
+           s21_fabs((double)(base - 1.0)) < epsilon)
     result = base;
 
   // Проверка на 0 в pow
@@ -34,9 +46,9 @@ long double s21_pow(double base, double exp) {
       exp--;
     }
 
-    result *= (double)s21_exp((exp * (double)s21_log(x754.d)));
+    result *= (double)s21_exp((exp * (double)s21_log(x754_base.d)));
 
-    if ((base < 0) && (s21_fabs((double)s21_fmod(exp, 2.0) - 1) < epsilon))
+    if ((base < 0) && (s21_fabs((double)s21_fmod(exp, 2.0) - 1.0) < epsilon))
       result *= -1;
   }
 
