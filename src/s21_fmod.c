c9fef780d895a771439c203fad908feb1e8c4df6
Ronin Shawanda <roninsha@su-f3.surgut.21-school.ru>
2023-07-12 17:34:08 +0500

    Fixed validation of variables.

--- src/s21_fmod.c
+++ src/s21_fmod.c
@@ -4,24 +4,55 @@ long double s21_fmod(double x, double y) {
   long long int mod = 0;
   long double result = 0;
 
+  union ieee754_double copy_x, copy_y, x754_full = {0};
 
-  // ВАРИАНТ_1
-  mod = (long long int)(x / y);
-  result = x - (double)mod * y;
+  // Создаем копии преходящих аргументов
+  copy_x.d = x;
+  copy_y.d = y;
 
-  // ВАРИАНТ_2
-  // long double mod = -1, temp = 0, result = 0;
-  // long double copy = s21_fabs(x);
+  // (1 << 11) - 1 = 2047
+  x754_full.ieee.exponent = (1 << 11) - 1;
 
-  // while (temp < copy) {
-  //   mod++;
-  //   temp += y;
-  // }
+  // Проверка х = nan
+  if ((copy_x.ieee_nan.negative == 0) &&
+      (copy_x.ieee_nan.exponent == x754_full.ieee.exponent) &&
+      (copy_x.ieee_nan.quiet_nan))
+    result = x;
 
-  // result = copy - mod * y;
+  // Проверка х = +-inf
+  else if ((copy_x.ieee_nan.exponent == x754_full.ieee.exponent) &&
+           (copy_x.ieee_nan.mantissa0 == 0))
+    result = -S21_NAN;
 
-  // if (x < 0)
-  //   result *= -1;
+  // Проверка y = +-nan
+  if ((copy_y.ieee_nan.exponent == x754_full.ieee.exponent) &&
+      (copy_y.ieee_nan.quiet_nan))
+    result = y; 
 
+  // Проверка y = +- inf
+  else if ((copy_y.ieee_nan.exponent == x754_full.ieee.exponent) &&
+           (copy_y.ieee_nan.mantissa0 == 0))
+    result = x;
+
+  // Любой другой вариант
+  else {
+    // ВАРИАНТ_1
+    mod = (long long int)(x / y);
+    result = x - (double)mod * y;
+
+    // ВАРИАНТ_2
+    // long double mod = -1, temp = 0, result = 0;
+    // long double copy = s21_fabs(x);
+
+    // while (temp < copy) {
+    //   mod++;
+    //   temp += y;
+    // }
+
+    // result = copy - mod * y;
+
+    // if (x < 0)
+    //   result *= -1;
+  }
   return (double)result;
 }
