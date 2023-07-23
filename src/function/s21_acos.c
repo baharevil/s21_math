adc14e3e7ab45f2868ed43cba2d9ef25dc4cb48d
Bloodislav <vladhamrik@gmail.com>
2023-07-23 14:18:41 +0500

    Clang-format

--- src/function/s21_acos.c
+++ src/function/s21_acos.c
@@ -2,7 +2,7 @@
 
 long double s21_acos(double x) {
   long double result = 0;
-  
+
   /*
     Область значения косинуса находять на промежутке от -1 до 1 включительно,
     поэтому х должен быть в данном прбежутке.
@@ -13,7 +13,8 @@ long double s21_acos(double x) {
   */
   if ((x > -1 && x < 1) || (s21_fabs((double)s21_fabs(x) - 1) < 1e-60))
     //Основная формула расчета: acos = pi/2 - atan(x / sqrt(1 - x^2))
-    result = S21_M_PI_2 - s21_atan(x / (double)s21_sqrt(1.0 - (double)s21_pow(x, 2)));
+    result = S21_M_PI_2 -
+             s21_atan(x / (double)s21_sqrt(1.0 - (double)s21_pow(x, 2)));
   else
     // define NAN возвращается отрицательным, поэтому юзаем fabs
     result = s21_fabs(S21_NAN);
